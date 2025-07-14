32cad04bddeb8feb23bef76f995aeec6605709b3
Jarves2025 <jonhydacosta@gmail.com>
2025-07-14 18:18:18 -0300

    Atualiza

--- clientes.js
+++ clientes.js
@@ -1,12 +1,12 @@
 // ------------ CLIENTES (SUPABASE) ------------
 
 // Usa o Supabase já criado no window (feito em utils.js OU no HTML antes dos outros scripts)
-if (!window.supabase) {
-    const SUPABASE_URL = 'https://jdflixpbupzwnictncbp.supabase.co';
-    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkZmxpeHBidXB6d25pY3RuY2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MDA4MjksImV4cCI6MjA2ODA3NjgyOX0.pNMVYVU5tw42_qMUhdJI1SE59xs5upVYz0RSyR81AMk';
-    window.supabase = window.supabase || supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-    const supabase = window.supabase;
-}
+const SUPABASE_URL = 'https://jdflixpbupzwnictncbp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkZmxpeHBidXB6d25pY3RuY2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MDA4MjksImV4cCI6MjA2ODA3NjgyOX0.pNMVYVU5tw42_qMUhdJI1SE59xs5upVYz0RSyR81AMk';
+
+window.supabase = window.supabase || supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabase = window.supabase;
+
 // Variável global
 let clientes = [];
 
